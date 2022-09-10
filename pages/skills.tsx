4e86ef6aecff88def10618b046eeba3dd05307fd
AbdulghaniHarran <abdulghani@rafraf.com>
2022-09-10 14:31:01 +0300

    skills+yarn

---
+++ pages/skills.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+function Skills() {
+  return (
+    <>
+    <div>
+        Skills
+    </div>
+        • Data science
+            # Analysing data using Python (Pandas, NumPy, Matplotlib) with SQL
+            # Familar with using mySQL databases, MongoDB.
+            # Expert at automating Excel functions, analysing and data visualizing.
+        • Software development - Frontend
+            # Javascript (Typescript), using Next.Js (React.Js)
+            # State mangement: Redux
+            # Apis: GraphQL, REST Api
+            # Version control: Git
+        • Software development - Backend
+        • Blockchain development
+        • Servers: Docker, Apache HTTP server
+    </>
+  )
+}
+
+export default Skills
